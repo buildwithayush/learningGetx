05bc7a372ae3974f08323fcedf7cdc2b1eccb264
Ayush Umrao <umraoayush@gmail.com>
2025-12-09 00:06:51 +0530

    Learning getx from mini todo app project

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -1,11 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getxlearn/screens/home.dart';
-import 'package:getxlearn/screens/maps_screen.dart';
-import 'package:getxlearn/screens/store_screen.dart';
 
 class HomeScreenController extends GetxController {
-  
-   RxInt selectedIndex = 0.obs;
-   List<Widget> screens = [const Home(),const MapsScreen(),const StoreScreen()];
+  static HomeScreenController get instance => Get.find();
+
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+
+  RxList data = [
+    {"title": "title 1", "description": "description 1"},
+    {"title": "title 2", "description": "description 2"},
+    {"title": "title 3", "description": "description 3"},
+    {"title": "title 4", "description": "description 4"},
+    {"title": "title 5", "description": "description 5"},
+  ].obs;
+
+  Future<void> addData() async {
+    await showModalBottomSheet(
+      context: Get.context!,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(9.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: titleController,
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  hintText: 'Enter title',
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 18,
+                    horizontal: 16,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade300,
+                      width: 1.2,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              TextFormField(
+                controller: descriptionController,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  hintText: 'Enter description...',
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 18,
+                    horizontal: 16,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade300,
+                      width: 1.2,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(onPressed: () {}, child: Text('Add')),
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,36 +3,34 @@ import 'package:get/get.dart';
 import 'package:getxlearn/controller/home_screen_controller.dart';
 
 
+
 class HomeScreen extends StatelessWidget {
-   HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   
 
   @override
   Widget build(BuildContext context) {
-    final HomeScreenController controller = Get.put(HomeScreenController());
+    final controller = Get.put(HomeScreenController());
+    
     return Scaffold(
-      
-      body: Obx(() => controller.screens[controller.selectedIndex.value]),
-      bottomNavigationBar: Obx(
-        () => NavigationBar(
-        
-          selectedIndex: controller.selectedIndex.value,
-        
-          onDestinationSelected: (value) {
-        
-            controller.selectedIndex.value = value;
-            
-          },
-          destinations: [
-        
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.search), label: 'Maps'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Store'),
-            
-          ],
-        ),
+      appBar: AppBar(
+        title: Text("GetX"),
+      ),
+      body:ListView.separated(
+       separatorBuilder: (context, index) => const SizedBox(),
+      itemCount: 10,
+      itemBuilder: (context, index){
+        return Text('1212');
+      },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.addData();
+        },
+        child: const Icon(Icons.add),
       ),
     );
+    
   }
 }
